e83bd149c2063993a13d2fb8fc5bc89fccded95a
aryanchari06 <aryaney0704@gmail.com>
2025-01-14 11:43:57 +0530

    debugging -4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,7 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace("/dashboard");
-  });
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       You are being redirected to the dashboard page...
