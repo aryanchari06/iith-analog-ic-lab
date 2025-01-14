0efbdf11ec7390f79edfe8dd66db503030be5e2a
aryanchari06 <aryaney0704@gmail.com>
2025-01-14 17:13:18 +0530

    input type password

--- src/app/(app)/sign-in/page.tsx
+++ src/app/(app)/sign-in/page.tsx
@@ -86,7 +86,7 @@ const Page = () => {
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
-                    <Input placeholder="password" {...field} />
+                    <Input placeholder="password" type="password" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
